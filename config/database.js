b15200846b580a6bc6eb2eeb5d2322a1473b37cd
surajpotnuru <surajpotnuru7@gmail.com>
2016-11-12 22:56:10 +0530

    remote database added

--- config/database.js
+++ config/database.js
@@ -2,11 +2,17 @@
  * Created by SURAJ on 11/10/2016.
  */
 var mysql = require("mysql");
+// var DBConnection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "afxbsqijmysql",
+//     database: "ojass"
+// });
 var DBConnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "afxbsqijmysql",
-    database: "ojass"
+    host: "http://mysql.hostinger.in",
+    user: "u414274941_ojass",
+    password: "ojass.mysql",
+    database: "u414274941_ojass"
 });
 
 var Database = module.exports = DBConnection;
